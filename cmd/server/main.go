5f419666bb0d02119c1fb72842fdac7fe887fbf4
AlkorMizar <anisa15012002@gmail.com>
2022-07-12 00:04:36 +0300

    added swaggo/swag #3

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,13 @@
 package main
+
+// @title           Swagger Example API
+// @version         1.0
+// @description     This is a sample server celler server.
+// @termsOfService  http://swagger.io/terms/
+
+// @host      localhost:8080
+// @BasePath  /
+
+// @securityDefinitions.apikey ApiKeyAuth
+//@in header
+//@name Auth
