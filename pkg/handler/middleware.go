dc2370f302ee471f2a02a0384e750caa0200e8f3
AlkorMizar <anisa15012002@gmail.com>
2022-07-15 14:57:02 +0300

    middleware checks header Authorization now #5

--- pkg/handler/middleware.go
+++ pkg/handler/middleware.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"context"
 	"net/http"
+	"strings"
 )
 
 type userInfo struct {
@@ -12,21 +13,44 @@ type userInfo struct {
 
 type ctxKey string
 
-var keyUserInfo = ctxKey("userInfo")
+var KeyUserInfo = ctxKey("userInfo")
+
+const (
+	authorizationHeaderKey  = "authorization"
+	authorizationTypeBearer = "bearer"
+	authorizationPayloadKey = "authorization_payload"
+)
 
 func (h *Handler) authentication(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		token, err := r.Cookie("Token")
-		if err != nil {
-			http.Error(w, "Forbidden", http.StatusForbidden)
+		token, ok := r.Header["Authorization"]
+		if !ok || len(token) != 1 {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
 			return
 		}
-		id, roles, err := h.services.Authorization.ParseToken(token.Value)
-		if err != nil {
-			http.Error(w, "Forbidden, please authorize", http.StatusForbidden)
+
+		fields := strings.Fields(token[0])
+		if len(fields) < 2 {
+			http.Error(w, "Invalid authorization header format", http.StatusForbidden)
 			return
 		}
-		ctx := context.WithValue(r.Context(), keyUserInfo, &userInfo{id, roles})
+
+		authorizationType := strings.ToLower(fields[0])
+		if authorizationType != authorizationTypeBearer {
+			http.Error(w, "Unsupported authorization type "+authorizationType, http.StatusForbidden)
+			return
+		}
+
+		accessToken := fields[1]
+
+		id, roles, err := h.services.Authorization.ParseToken(accessToken)
+		if err != nil {
+			http.Error(w, "Forbidden, please authorize again", http.StatusForbidden)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), KeyUserInfo, userInfo{id, roles})
+
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
