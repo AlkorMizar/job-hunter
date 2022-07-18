0d98ef288a32bbcbeae363d7eb24b7eadec82965
AlkorMizar <anisa15012002@gmail.com>
2022-07-19 01:19:13 +0300

    tests for handler updateUser #13

--- pkg/handler/profile_test.go
+++ pkg/handler/profile_test.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -48,7 +49,7 @@ func TestGetUser(t *testing.T) {
 			http.StatusInternalServerError,
 		},
 		{
-			"ok",
+			"invalid userIfo",
 			&struct {
 				id string
 			}{
@@ -118,6 +119,193 @@ func TestGetUser(t *testing.T) {
 
 			if user.Login != userNeed.Login {
 				t.Fatalf("%s:incorrect data format : got %v want %v", test.name, user, userNeed)
+			}
+		})
+	}
+}
+
+func TestUpdateUser(t *testing.T) {
+	uInfo := userInfo{
+		1,
+		make(map[string]struct{}),
+	}
+
+	tests := []struct {
+		name               string
+		userInf            interface{}
+		newInf             model.UpdateInfo
+		mock               func(id int, inf model.UpdateInfo) error
+		expectedStatusCode int
+	}{
+		{
+			"ok, all fields",
+			uInfo,
+			model.UpdateInfo{
+				Login: "login", Email: "tesd@fsd.com", FullName: "Fluff Puff",
+			},
+			func(id int, inf model.UpdateInfo) error {
+				return nil
+			},
+			http.StatusOK,
+		},
+		{
+			"ok, one field",
+			uInfo,
+			model.UpdateInfo{
+				Email: "tesd@fsd.com",
+			},
+			func(id int, inf model.UpdateInfo) error {
+				return nil
+			},
+			http.StatusOK,
+		},
+		{
+			"ok, two fields",
+			uInfo,
+			model.UpdateInfo{
+				Login: "login", FullName: "Fluff Puff",
+			},
+			func(id int, inf model.UpdateInfo) error {
+				return nil
+			},
+			http.StatusOK,
+		},
+		{
+			"ok, all fields",
+			userInfo{
+				1,
+				make(map[string]struct{}),
+			},
+			model.UpdateInfo{
+				Login: "login", Email: "tesd@fsd.com", FullName: "Fluff Puff",
+			},
+			func(id int, inf model.UpdateInfo) error {
+				return nil
+			},
+			http.StatusOK,
+		},
+		{
+			"bad request body",
+			userInfo{
+				1,
+				make(map[string]struct{}),
+			},
+			model.UpdateInfo{
+				Login: "lo", Email: "tesdfsd.com", FullName: "f",
+			},
+			func(id int, inf model.UpdateInfo) error {
+				return fmt.Errorf("bad request body")
+			},
+			http.StatusBadRequest,
+		},
+		{
+			"bad request userInfo",
+			userInfo{
+				-1,
+				make(map[string]struct{}),
+			},
+			model.UpdateInfo{
+				Login: "login", Email: "tesd@fsd.com", FullName: "Fluff Puff",
+			},
+			func(id int, inf model.UpdateInfo) error {
+				return fmt.Errorf("bad request userInfo")
+			},
+			http.StatusBadRequest,
+		},
+		{
+			"invalid userInfo",
+			&struct {
+				id string
+			}{
+				"1",
+			},
+			model.UpdateInfo{
+				Login: "login", Email: "tesd@fsd.com", FullName: "Fluff Puff",
+			},
+			func(id int, inf model.UpdateInfo) error {
+				return nil
+			},
+			http.StatusBadRequest,
+		},
+		{
+			"internal error during service process",
+			userInfo{
+				1,
+				make(map[string]struct{}),
+			},
+			model.UpdateInfo{
+				Login: "lddddo", Email: "tesdf@sd.com", FullName: "fdddddd",
+			},
+			func(id int, inf model.UpdateInfo) error {
+				return fmt.Errorf("internal error")
+			},
+			http.StatusInternalServerError,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+
+			service := &service.Service{
+				User: &mock.UserServiceMock{
+					MockUpdateUSer: test.mock,
+				},
+			}
+
+			handler := Handler{service}
+
+			body, err := json.Marshal(test.newInf)
+
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			req, err := http.NewRequest("PUT", "/user", bytes.NewBuffer(body))
+
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			ctx := context.WithValue(req.Context(), KeyUserInfo, test.userInf)
+
+			req = req.WithContext(ctx)
+
+			rr := httptest.NewRecorder()
+
+			reg := http.HandlerFunc(handler.updateUser)
+
+			reg.ServeHTTP(rr, req)
+
+			// Check the status code is what we expect.
+			if status := rr.Code; status != test.expectedStatusCode {
+				t.Fatalf("%s:handler returned wrong status code: got %v want %v",
+					test.name, status, test.expectedStatusCode)
+			}
+
+			if rr.Body.Len() <= 0 {
+				t.Fatal("no body in response")
+			}
+
+			bodyResp := model.JSONResult{}
+
+			err = json.NewDecoder(rr.Body).Decode(&bodyResp)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if bodyResp.Data == nil {
+				return
+			}
+
+			updatedInfo := model.UpdateInfo{}
+			err = mapstructure.Decode(bodyResp.Data, &updatedInfo)
+
+			if err != nil {
+				t.Fatalf("%s:incorrect data format get %v", test.name, bodyResp)
+			}
+
+			if updatedInfo != test.newInf {
+				t.Fatalf("%s:incorrect data format : got %v want %v", test.name, updatedInfo, test.newInf)
 			}
 		})
 	}
--- pkg/service/mock/profile.go
+++ pkg/service/mock/profile.go
@@ -3,7 +3,8 @@ package mock
 import "github.com/AlkorMizar/job-hunter/pkg/handler/model"
 
 type UserServiceMock struct {
-	MockGetUser func(id int) (user *model.User, err error)
+	MockGetUser    func(id int) (user *model.User, err error)
+	MockUpdateUSer func(id int, inf model.UpdateInfo) error
 }
 
 func (u *UserServiceMock) GetUser(id int) (user *model.User, err error) {
@@ -11,5 +12,5 @@ func (u *UserServiceMock) GetUser(id int) (user *model.User, err error) {
 }
 
 func (u *UserServiceMock) UpdateUser(id int, inf model.UpdateInfo) error {
-	return nil
+	return u.MockUpdateUSer(id, inf)
 }
