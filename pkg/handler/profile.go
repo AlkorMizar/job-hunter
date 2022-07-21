aa92af44a8089a887a4b220c8bfdb03fd6b53e82
AlkorMizar <anisa15012002@gmail.com>
2022-07-21 15:28:26 +0300

    Revert "handler for updateIser #13"
    This reverts commit 32c50de1920596b761dfab47e272684ce42cf612.

--- pkg/handler/profile.go
+++ pkg/handler/profile.go
@@ -3,10 +3,8 @@ package handler
 import (
 	"encoding/json"
 	"net/http"
-	"strings"
 
 	"github.com/AlkorMizar/job-hunter/pkg/handler/model"
-	"github.com/go-playground/validator"
 )
 
 // @Summary      Get User
@@ -50,7 +48,6 @@ func (h *Handler) getUser(w http.ResponseWriter, r *http.Request) {
 // @Description  Changes users' login, email, full name
 // @Security     ApiKeyAuth
 // @Tags         user
-// @Accept       json
 // @Produce      json
 // @Param        newInfo    body 	   model.UpdateInfo true "Login, email, full name"
 // @Success      200  		{object}   model.JSONResult
@@ -58,60 +55,5 @@ func (h *Handler) getUser(w http.ResponseWriter, r *http.Request) {
 // @Failure      500  		{object}   model.JSONResult
 // @Router       /user [put]
 func (h *Handler) updateUser(w http.ResponseWriter, r *http.Request) {
-	userInf, ok := r.Context().Value(KeyUserInfo).(userInfo)
-	if !ok {
-		writeErrResp(w, "users' info is invalid", http.StatusBadRequest)
 
-		return
-	}
-
-	validate := validator.New()
-
-	decoder := json.NewDecoder(r.Body)
-
-	var update model.UpdateInfo
-
-	err := decoder.Decode(&update)
-
-	if err != nil {
-		writeErrResp(w, "incorrect data format", http.StatusBadRequest)
-
-		return
-	}
-
-	err = validate.Struct(update)
-
-	if err != nil {
-		writeErrResp(w, "incorrect fields", http.StatusBadRequest)
-
-		return
-	}
-
-	if update.FullName != "" {
-		update.FullName = strings.TrimSpace(update.FullName)
-		if update.FullName == "" {
-			writeErrResp(w, "full name empty", http.StatusBadRequest)
-
-			return
-		}
-	}
-
-	err = h.services.UpdateUser(userInf.id, update)
-	if err != nil {
-		writeErrResp(w, "internal error", http.StatusInternalServerError)
-
-		return
-	}
-
-	body := model.JSONResult{
-		Message: "Successfully authorized",
-		Data:    nil,
-	}
-
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/json")
-
-	if err := json.NewEncoder(w).Encode(body); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-	}
 }
