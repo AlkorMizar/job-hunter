f7be5a79c05d873e5444fb1f44b3819392ad44c6
AlkorMizar <anisa15012002@gmail.com>
2022-07-21 15:33:33 +0300

    Revert "User service structure and getUser method #12"
    This reverts commit 21970d88fb5f3db57c7d45ea8e86f359e8de4584.

--- pkg/service/profile.go
+++ pkg/service/profile.go
@@ -1,19 +1,1 @@
 package service
-
-import (
-	"github.com/AlkorMizar/job-hunter/pkg/repository"
-)
-
-type UserServ struct {
-	repo repository.UserManagment
-}
-
-func NewUserServ(repo repository.UserManagment) *UserServ {
-	return &UserServ{
-		repo: repo,
-	}
-}
-
-func (u *UserServ) GetUser(id int) (user *repository.User, err error) {
-	return &repository.User{}, nil
-}
