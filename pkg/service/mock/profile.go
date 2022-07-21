40467988f22816e3180df0fea784fd5eb2a8d702
AlkorMizar <anisa15012002@gmail.com>
2022-07-21 15:28:27 +0300

    Revert "tests for handler updateUser #13"
    This reverts commit 0d98ef288a32bbcbeae363d7eb24b7eadec82965.

--- pkg/service/mock/profile.go
+++ pkg/service/mock/profile.go
@@ -3,8 +3,7 @@ package mock
 import "github.com/AlkorMizar/job-hunter/pkg/handler/model"
 
 type UserServiceMock struct {
-	MockGetUser    func(id int) (user *model.User, err error)
-	MockUpdateUSer func(id int, inf model.UpdateInfo) error
+	MockGetUser func(id int) (user *model.User, err error)
 }
 
 func (u *UserServiceMock) GetUser(id int) (user *model.User, err error) {
@@ -12,5 +11,5 @@ func (u *UserServiceMock) GetUser(id int) (user *model.User, err error) {
 }
 
 func (u *UserServiceMock) UpdateUser(id int, inf model.UpdateInfo) error {
-	return u.MockUpdateUSer(id, inf)
+	return nil
 }
