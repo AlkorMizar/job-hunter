6abb8bd8fa296c0ccfadeabc0ef874aef8155570
AlkorMizar <anisa15012002@gmail.com>
2022-07-24 00:34:57 +0300

    mock interface #18

--- internal/repository/mock/user.go
+++ internal/repository/mock/user.go
@@ -3,17 +3,17 @@ package mock
 import "github.com/AlkorMizar/job-hunter/internal/repository"
 
 type UserManagment struct {
-	MockCreateUser func(user *repository.User) error
-	MockGetUser    func(email string) (repository.User, error)
-	MockGetRoles   func(user *repository.User) (map[string]struct{}, error)
-	MockSetRoles   func(user *repository.User) error
+	MockCreateUser       func(user *repository.User) error
+	MockGetUserWithEamil func(email string) (repository.User, error)
+	MockGetRoles         func(user *repository.User) (map[string]struct{}, error)
+	MockSetRoles         func(user *repository.User) error
 }
 
 func (u *UserManagment) CreateUser(user *repository.User) error {
 	return u.MockCreateUser(user)
 }
-func (u *UserManagment) GetUser(email string) (repository.User, error) {
-	return u.MockGetUser(email)
+func (u *UserManagment) GetUserWithEamil(email string) (repository.User, error) {
+	return u.MockGetUserWithEamil(email)
 }
 func (u *UserManagment) GetRoles(user *repository.User) (map[string]struct{}, error) {
 	return u.MockGetRoles(user)
